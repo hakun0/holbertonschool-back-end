4b335546e966540cab9b28d9889655130afc69bf
hakun0 <brithon.saniko@gmail.com>
2024-04-19 22:23:27 +0200

    ledutredutro

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Returns info about an employee's TODO list using REST API."""
+import json
+import requests
+if __name__ == "__main__":
+    link = "https://jsonplaceholder.typicode.com"
+    employees = requests.get(f"{link}/users").json()
+    all_employees_tasks = {}
+    for employee in employees:
+        employee_id = employee.get("id")
+        todo_list = requests.get(f"{link}/todos?userId={employee_id}").json()
+        employee_tasks = [{"username": employee.get("username"),
+                           "task": todo.get("title"),
+                           "completed": todo.get("completed")}
+                          for todo in todo_list]
+        all_employees_tasks[employee_id] = employee_tasks
+    with open("todo_all_employees.json", mode="w", encoding="utf-8") as f:
+        json.dump(all_employees_tasks, f)
